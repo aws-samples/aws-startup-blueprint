0a0307423f252d3060b387391918a8c59de0b8a4
Paul Underwood <paulu@amazon.com>
2020-10-06 10:50:13 -0500

    updating template description

--- bin/aws-startup-blueprint.ts
+++ bin/aws-startup-blueprint.ts
@@ -4,6 +4,6 @@ import * as cdk from '@aws-cdk/core';
 import { AwsStartupBlueprintStack } from '../lib/aws-startup-blueprint-stack';
 
 const app = new cdk.App();
-new AwsStartupBlueprintStack(app, 'AwsStartupBlueprintStack', {
-    description: "The AWS Startup Blueprint is a strongly opinionated architecture for any Startup looking to start using AWS follwing best practices on day 1. (ib-7061756C75)"
+new AwsStartupBlueprintStack(app, 'AwsBiotechBlueprint', {
+    description: "The AWS Biotech Blueprint is a strongly opinionated architecture for any Startup looking to start using AWS follwing best practices on day 1. (qs-1of009lua)"
 });
