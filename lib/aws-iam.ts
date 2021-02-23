baa50eab76bb66e36f154a983bf811354214ab35
Colgan <colgc@147dda0c2505.ant.amazon.com>
2021-02-23 12:12:15 -0700

    Correct the managed policy

--- lib/aws-iam.ts
+++ lib/aws-iam.ts
@@ -77,7 +77,7 @@ const policyDocument = {
 };
 
 const customPolicyDocument = iam.PolicyDocument.fromJson(policyDocument);
-const newManagedPolicy = new ManagedPolicy(stack, 'DiGavPermissionBoundaryPolicy', {
+const newManagedPolicy = new iam.ManagedPolicy(AwsStartupBlueprintStack, 'DiGavPermissionBoundaryPolicy', {
     document: customPolicyDocument
 });
 
@@ -85,7 +85,7 @@ export class PermissionBoundary extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id);
     
-    iam.PermissionsBoundary.of(stack).apply(newManagedPolicy)
+    iam.PermissionsBoundary.of(AwsStartupBlueprintStack).apply(newManagedPolicy)
     }
 
 }
