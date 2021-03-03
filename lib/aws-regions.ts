f6f8f32511e6c26c81f430ed0f026b223c1a06e3
Colgan <colgc@147dda0c2505.ant.amazon.com>
2021-03-03 11:31:30 -0700

    Correct syntax

--- lib/aws-regions.ts
+++ lib/aws-regions.ts
@@ -8,30 +8,31 @@ export class RegionRestriction extends cdk.Construct {
 	constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id);
 
-        const regionsFn = new lambda.Function(this, 'regionsFn', {
-        runtime: lambda.Runtime.PYTHON_3_7,
-        code: lambda.Code.fromAsset('./scripts/'),
-        handler: 'restrictregionpolicy-customlambdaresource.handler',
-    });
+            const regionsFn = new lambda.Function(this, 'regionsFn', {
+            runtime: lambda.Runtime.PYTHON_3_7,
+            code: lambda.Code.fromAsset('./scripts/'),
+            handler: 'restrictregionpolicy-customlambdaresource.handler',
+        });
 
-    if (regionsFn.role) {
-        regionsFn.role.addToPrincipalPolicy(new iam.PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: ['iam.CreateRole', 'iam.CreatePolicy', 'iam.AddRoleToInstanceProfile', 'iam.AttachRolePolicy', 'iam.CreateInstanceProfile', 
-            'iam.CreatePolicyVersion', 'iam.DeleteInstanceProfile', 'iam.DeletePolicy', 'iam.DeletePolicyVersion', 'iam.DeleteRole', 'iam.DeleteRolePermissionsBoundary', 
-            'iam.DeleteRolePolicy', 'iam.DetachRolePolkicy', 'iam.Get*', 'iam.List*', 'iam.RemoveRoleFromInstanceProfile', 'iam.PutRolePermissionBoundary', 'iam.PutRolePolicy',
-            'organizations.AttachPolicy', 'organizations.CreatePolicy', 'organizations.DeletePolicy', 'organizations.DescribeOrganization', 'organizations.DescribePolicy', 
-            'organizations.DetachPolicy', 'organizations.ListPolicies*', 'organizations.UpdatePolicy'],
-            resources: ['*'],
-        }));
+        if (regionsFn.role) {
+            regionsFn.role.addToPrincipalPolicy(new iam.PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ['iam.CreateRole', 'iam.CreatePolicy', 'iam.AddRoleToInstanceProfile', 'iam.AttachRolePolicy', 'iam.CreateInstanceProfile', 
+                'iam.CreatePolicyVersion', 'iam.DeleteInstanceProfile', 'iam.DeletePolicy', 'iam.DeletePolicyVersion', 'iam.DeleteRole', 'iam.DeleteRolePermissionsBoundary', 
+                'iam.DeleteRolePolicy', 'iam.DetachRolePolkicy', 'iam.Get*', 'iam.List*', 'iam.RemoveRoleFromInstanceProfile', 'iam.PutRolePermissionBoundary', 'iam.PutRolePolicy',
+                'organizations.AttachPolicy', 'organizations.CreatePolicy', 'organizations.DeletePolicy', 'organizations.DescribeOrganization', 'organizations.DescribePolicy', 
+                'organizations.DetachPolicy', 'organizations.ListPolicies*', 'organizations.UpdatePolicy'],
+                resources: ['*'],
+            }));
+        }
+
+        const regionsFnProvider = new customresources.Provider(this, 'RegionFnCustomResource', {
+            onEventHandler: regionsFn,
+        });
+
+        const CustomResource = new CustomResource(this, 'regionCustomResource', {
+            serviceToken: regionsFn.functionArn,
+            properties: {},
+        });
     }
-
-    const regionsFnProvider = new customresources.Provider(this, 'RegionFnCustomResource', {
-        onEventHandler: regionsFn,
-    });
-
-    const CustomResource = new CustomResource(this, 'regionCustomResource', {
-        serviceToken: regionsFn.functionArn,
-        properties: {},
-    });
 }
