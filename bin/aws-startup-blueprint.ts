672823579c05e318e55b70fde649197f4841a34f
Colgan <colgc@147dda0c2505.ant.amazon.com>
2021-03-12 09:03:12 -0700

    Update stack name

--- bin/aws-startup-blueprint.ts
+++ bin/aws-startup-blueprint.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AwsStartupBlueprintStack } from '../lib/aws-startup-blueprint-stack';
+import { AwsDiGAVBlueprintStack } from '../lib/aws-startup-blueprint-stack';
 
 const app = new cdk.App();
-new AwsStartupBlueprintStack(app, 'AwsBiotechBlueprint', {
-    description: "AWS Biotech Blueprint CDK is an AWS Quick Start that helps Biotech companies deploy core AWS Infrastructure as well as CloudFormation templates for common ISV solutions. (qs-1of009lua) (ib-1of009lua)"
+new AwsStartupBlueprintStack(app, 'AwsDiGAVBlueprint', {
+    description: "AWS DiGAV Blueprint CDK is an AWS Quick Start that helps companies deploy core AWS Infrastructure as well as CloudFormation templates that may help meet DiGAV requirements."
 });
